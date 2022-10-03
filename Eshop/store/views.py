1c3b8b6200ca7ea49206be21143544c5875e056a
KXR124 <karthik.12420052@gmail.com>
2022-10-03 10:09:00 +0530

    git commit

---
+++ Eshop/store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models.product import Product
+from .models.Category import Category 
+
+# Create your views here.
+def index(request):
+    products= Product.get_all_products();
+    categories = Category.get_all_categories();
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
